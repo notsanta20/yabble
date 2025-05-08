84611265aef7492d8b241e826e424fbc1e391a9d
santhosh <santhosh.003z@gmail.com>
2025-05-09 00:29:35 +0530

    feat: added button component

---
+++ src/components/ui/buttons/Button.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+
+interface props {
+  url: string;
+  name: string;
+}
+
+function Button({ url, name }: props) {
+  return (
+    <button className="p-4 rounded-2xl font-[Syncopate] text-xl font-bold text-white border-2 border-(--glass-border-dark) bg-(--glass-fill-dark) backdrop-blur-(--glass-blur) hover:bg-(--glass-fill-white) cursor-pointer">
+      <Link to={url} />
+      {name}
+    </button>
+  );
+}
+
+export default Button;
