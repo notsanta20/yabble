8ad6a774203e9b2374feb134b859788a3ca00975
santhosh <santhosh.003z@gmail.com>
2025-05-09 00:38:43 +0530

    fix: fixed link tag in button

--- src/components/ui/buttons/Button.tsx
+++ src/components/ui/buttons/Button.tsx
@@ -8,8 +8,7 @@ interface props {
 function Button({ url, name }: props) {
   return (
     <button className="p-4 rounded-2xl font-[Syncopate] text-xl font-bold text-white border-2 border-(--glass-border-dark) bg-(--glass-fill-dark) backdrop-blur-(--glass-blur) hover:bg-(--glass-fill-white) cursor-pointer">
-      <Link to={url} />
-      {name}
+      <Link to={url}>{name}</Link>
     </button>
   );
 }
