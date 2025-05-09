1c46cafef4b2d12a4e986f9911e2e41c5eb38fc4
santhosh <santhosh.003z@gmail.com>
2025-05-09 21:38:28 +0530

    feat: added types

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export interface SignFormData {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface LoginFormData {
+  username: string;
+  email: string;
+  password: string;
+}
