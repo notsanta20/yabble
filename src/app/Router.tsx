fa45fa17ed56a0db00f12ebc4932f58d5cc81daf
santhosh <santhosh.003z@gmail.com>
2025-05-09 23:54:06 +0530

    feat: added home page route and login API

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router";
 import Landing from "./routes/Landing";
 import Signup from "./routes/auth/Signup";
 import Login from "./routes/auth/Login";
+import Lobby from "./routes/app/Lobby";
+import Home from "./routes/app/Home";
 
 function Router() {
   return (
@@ -9,7 +11,9 @@ function Router() {
       <Route path="/" element={<Landing />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route></Route>
+      <Route path="/home" element={<Lobby />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
