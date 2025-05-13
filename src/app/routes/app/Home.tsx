3bacb7dbb8abdfd239d170ddce78ddd02a0391e6
santhosh <santhosh.003z@gmail.com>
2025-05-13 14:10:58 +0530

    feat: added post wall

--- src/app/routes/app/Home.tsx
+++ src/app/routes/app/Home.tsx
@@ -1,13 +1,18 @@
 import { HeadingSmall } from "../../../components/texts/Heading";
 import Menu from "../../../components/ui/menu/Menu";
+import PostWall from "../../../components/ui/Post/PostWall";
+import ActiveUsers from "../../../components/ui/users/ActiveUsers";
 
 function Home() {
   return (
-    <main className="flex flex-col gap-2 p-2">
+    <main className="flex flex-col gap-2 p-2 h-screen">
       <HeadingSmall />
       <div className="flex-1 flex gap-2">
-        <Menu />
-        <div className="flex-1">Home</div>
+        <Menu name="home" />
+        <div className="flex-1">
+          <PostWall />
+        </div>
+        <ActiveUsers />
       </div>
     </main>
   );
