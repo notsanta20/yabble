cb44ec378b783e361805caf6036e87ac1ce08a88
santhosh <santhosh.003z@gmail.com>
2025-05-13 15:56:12 +0530

    fix: added token into the get header function

--- src/utils/apis/getRequests.ts
+++ src/utils/apis/getRequests.ts
@@ -1,21 +1,27 @@
 import Cookies from "js-cookie";
 import axios from "axios";
+import type { Header } from "../../types/types";
 
 const url: string = "http://localhost:3000";
-const token: string | undefined = Cookies.get("token");
-const Authorization: string =
-  typeof token === "undefined" ? "unauthorized" : `Bearer ${token}`;
-const header = {
-  headers: { Authorization },
-};
 
-export async function activeUsersApi() {
+export function getHeader() {
+  const token: string | undefined = Cookies.get("token");
+
+  const Authorization: string =
+    typeof token === "undefined" ? "unauthorized" : `Bearer ${token}`;
+
+  return {
+    headers: { Authorization },
+  };
+}
+
+export async function activeUsersApi(header: Header) {
   const data = await axios.get(`${url}/active-users`, header);
 
   return data;
 }
 
-export async function allPostsApi() {
+export async function allPostsApi(header: Header) {
   const data = await axios.get(`${url}/posts`, header);
 
   return data;
