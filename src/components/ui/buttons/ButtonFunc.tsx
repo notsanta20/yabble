f4cbb2e70c34d4b3354f5508709e99082e3454b1
santhosh <santhosh.003z@gmail.com>
2025-05-16 00:15:00 +0530

    feat: added is pending status for the button, added loader and disabled functionality

--- src/components/ui/buttons/ButtonFunc.tsx
+++ src/components/ui/buttons/ButtonFunc.tsx
@@ -2,23 +2,35 @@ function ButtonFunc({
   text,
   callback,
   page,
+  isPending,
 }: {
   text: string;
   callback?: Function;
   page?: string;
+  isPending: boolean;
 }) {
   return (
     <button
       className={
-        "font-[Syncopate] text-white text-xs font-bold p-3 rounded-2xl border-2 border-(--glass-border-light)" +
+        "font-[Syncopate] text-white text-xs font-bold p-3 rounded-2xl border-2 border-(--glass-border-light) flex justify-center" +
         (callback ? " hover:bg-(--glass-fill-light) cursor-pointer" : "") +
-        (page === text ? " bg-(--glass-fill-dark)" : "")
+        (page === text ? " bg-(--glass-fill-dark)" : "") +
+        (isPending ? " w-[100px]" : "")
       }
       onClick={(e) => {
         callback && callback(e);
       }}
+      disabled={isPending}
     >
-      {text}
+      {isPending ? (
+        <img
+          src="/assets/loaders/spinner.svg"
+          alt="loader"
+          className="w-[20px] h-auto"
+        />
+      ) : (
+        text
+      )}
     </button>
   );
 }
