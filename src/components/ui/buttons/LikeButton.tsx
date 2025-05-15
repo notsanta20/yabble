329e55b8bd1796965f833b03c981c37ca4a2e678
santhosh <santhosh.003z@gmail.com>
2025-05-16 00:41:37 +0530

    fix: added error type for query error

--- src/components/ui/buttons/LikeButton.tsx
+++ src/components/ui/buttons/LikeButton.tsx
@@ -1,6 +1,6 @@
 import { getHeader, addLikeApi } from "../../../utils/apis/putRequests";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import type { Header } from "../../../types/types";
+import type { Error, Header } from "../../../types/types";
 import alert from "../alert/alert";
 
 function LikeButton({
@@ -14,19 +14,19 @@ function LikeButton({
 }) {
   const queryClient = useQueryClient();
   const header = getHeader();
+
   const { mutate } = useMutation({
     mutationKey: ["addLike", header],
     mutationFn: ({ id, header }: { id: string; header: Header }) => {
       return addLikeApi({ id, header });
     },
-    onSettled: (error) => {
-      if (error) {
-        alert(error.response.data.message);
-      } else {
-        queryClient.invalidateQueries({ queryKey: ["allPost"] });
-        queryClient.invalidateQueries({ queryKey: ["post"] });
-        queryClient.invalidateQueries({ queryKey: ["currentUser"] });
-      }
+    onError: (error: Error) => {
+      alert(error.response.data.message);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["allPost"] });
+      queryClient.invalidateQueries({ queryKey: ["post"] });
+      queryClient.invalidateQueries({ queryKey: ["currentUser"] });
     },
   });
 
