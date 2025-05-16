b138d03440ec609e81cb6d5d9a2c042b11718518
santhosh <santhosh.003z@gmail.com>
2025-05-17 00:35:51 +0530

    feat: moved friends list under menu and added outlet for nested route

--- src/app/routes/app/Messages.tsx
+++ src/app/routes/app/Messages.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { HeadingSmall } from "../../../components/texts/Heading";
 import Menu from "../../../components/ui/menu/Menu";
+import MessageUsersWall from "../../../components/ui/message/MessageUsersWall";
+import { Outlet } from "react-router";
 
 function Messages() {
   return (
-    <main className="flex flex-col gap-2 p-2">
+    <main className="flex flex-col gap-2 p-2 h-dvh">
       <HeadingSmall />
       <div className="flex-1 flex gap-2">
-        <Menu name="messages" />
-        <div className="flex-1">Messages</div>
+        <div className="flex flex-col gap-2">
+          <Menu name="messages" />
+          <MessageUsersWall />
+        </div>
+        <section className="flex-auto flex gap-2">
+          <Outlet />
+        </section>
       </div>
     </main>
   );
