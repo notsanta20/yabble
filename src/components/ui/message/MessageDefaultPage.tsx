2ee1aa894ad56eab96c4c36a79da738e70a424e9
santhosh <santhosh.003z@gmail.com>
2025-05-17 00:34:50 +0530

    feat: added default page for messages view

---
+++ src/components/ui/message/MessageDefaultPage.tsx
@@ -0,0 +1,9 @@
+function MessageDefaultPage() {
+  return (
+    <section className="flex-auto flex justify-center items-center">
+      <h1 className="text-white font-[Syncopate] font-bold">START CHATTING</h1>
+    </section>
+  );
+}
+
+export default MessageDefaultPage;
