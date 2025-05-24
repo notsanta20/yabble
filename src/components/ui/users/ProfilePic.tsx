0307b06cf66f7812ee65df1b2c29bff3f162760a
santhosh <santhosh.003z@gmail.com>
2025-05-24 18:17:18 +0530

    fix: fixed image to cover the div and fixed border roundness

--- src/components/ui/users/ProfilePic.tsx
+++ src/components/ui/users/ProfilePic.tsx
@@ -3,8 +3,12 @@ import type { BasicUser, FindFriendsUser } from "../../../types/types";
 function ProfilePic({ user }: { user: BasicUser | FindFriendsUser }) {
   if (user.profilePic) {
     return (
-      <div className=" rounded-full w-[25px] h-[25px] flex justify-center items-center">
-        <img src={user.profilePic} alt="profile-pic" />
+      <div className="w-[25px] h-[25px] flex justify-center items-center">
+        <img
+          src={user.profilePic}
+          alt="profile-pic"
+          className="w-full h-full object-cover rounded-full"
+        />
       </div>
     );
   }
