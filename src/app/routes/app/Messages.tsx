7f6bc2f782b21c5cc6f5733753266bc8f0c32851
santhosh <santhosh.003z@gmail.com>
2025-05-29 22:21:53 +0530

    feat: added socket connection

--- src/app/routes/app/Messages.tsx
+++ src/app/routes/app/Messages.tsx
@@ -2,12 +2,19 @@ import { HeadingSmall } from "../../../components/texts/Heading";
 import Menu from "../../../components/ui/menu/Menu";
 import MessageUsersWall from "../../../components/ui/message/MessageUsersWall";
 import { Outlet } from "react-router";
+import socket from "../../socket";
+import { getHeader } from "../../../utils/apis/getRequests";
 
 function Messages() {
+  const header = getHeader();
+  const token = header.headers.Authorization;
+  socket.auth = { token };
+  socket.connect();
+
   return (
     <main className="flex flex-col gap-2 p-2 h-dvh">
       <HeadingSmall />
-      <div className="flex-auto flex gap-2">
+      <div className="flex-auto flex gap-2 min-h-0">
         <div className="max-sm:flex-auto flex flex-col-reverse md:flex-col gap-2">
           <Menu name="messages" />
           <MessageUsersWall />
