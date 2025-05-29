87432a0045614ce9b359dad3256cc7bdd27ce4f0
santhosh <santhosh.003z@gmail.com>
2025-05-29 22:14:45 +0530

    fix: fixed overflow issue and type issue for user

--- src/components/ui/users/FindFriendsWall.tsx
+++ src/components/ui/users/FindFriendsWall.tsx
@@ -2,6 +2,7 @@ import FindFriendCard from "./FindFriendCard";
 import { getHeader, getAllUsersApi } from "../../../utils/apis/getRequests";
 import { useQuery } from "@tanstack/react-query";
 import FindFriendLoader from "../loaders/FindFriendLoader";
+import type { FindFriendsUser } from "../../../types/types";
 
 function FindFriendsWall() {
   const header = getHeader();
@@ -48,15 +49,17 @@ function FindFriendsWall() {
   if (allUsers.data) {
     const data = allUsers.data.data.data;
     return (
-      <section className="flex-1 flex md:justify-center">
-        <ul className="h-full w-full md:w-[50%] flex flex-col gap-2 rounded-2xl border-2 border-(--glass-border-light) bg-(--glass-fill-light) backdrop-blur-(--glass-blur) p-2">
+      <section className="flex-auto flex md:justify-center min-h-0">
+        <div className="w-full md:w-[50%] flex flex-col gap-2 rounded-2xl border-2 border-(--glass-border-light) bg-(--glass-fill-light) backdrop-blur-(--glass-blur) p-2">
           <h1 className="text-white text-center font-bold font-[Syncopate] mb-2">
             Find Friends
           </h1>
-          {data.map((user) => (
-            <FindFriendCard user={user} key={user.id} />
-          ))}
-        </ul>
+          <ul className="flex-auto flex flex-col gap-2 overflow-auto request-container">
+            {data.map((user: FindFriendsUser) => (
+              <FindFriendCard user={user} key={user.id} />
+            ))}
+          </ul>
+        </div>
       </section>
     );
   }
