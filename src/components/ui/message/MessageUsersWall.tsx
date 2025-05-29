ce1a73557e6a040cb4116c0bc28013dc5d3f8763
santhosh <santhosh.003z@gmail.com>
2025-05-29 22:25:36 +0530

    feat: removed api call and intergrated socket

--- src/components/ui/message/MessageUsersWall.tsx
+++ src/components/ui/message/MessageUsersWall.tsx
@@ -1,59 +1,60 @@
-import { useState } from "react";
-import type { Error, FriendList } from "../../../types/types";
-import { getHeader, getFriendsList } from "../../../utils/apis/getRequests";
+import { useState, useEffect } from "react";
+import type { FriendList } from "../../../types/types";
 import alert from "../alert/alert";
 import MessageUser from "./MessageUser";
-import { useQuery } from "@tanstack/react-query";
+import socket from "../../../app/socket";
+import ActiveUserLoader from "../loaders/ActiveUserLoader";
 
 function MessageUsersWall() {
   const [userMenu, setUserMenu] = useState<string | null>(null);
-  const header = getHeader();
+  const [users, setUsers] = useState<Array<FriendList> | null>(null);
 
-  const friends = useQuery({
-    queryKey: ["friend"],
-    queryFn: () => {
-      return getFriendsList(header);
-    },
+  useEffect(() => {
+    socket.emit("getUsers");
+    socket.on("users", (data) => {
+      setUsers(data);
+    });
+  }, []);
+
+  socket.on("error", (error) => {
+    alert(error);
   });
 
   function handleUserMenuHighlight(name: string) {
     setUserMenu(name);
   }
 
-  if (friends.isPending) {
-  }
-
-  if (friends.error) {
-    const error: Error = friends.error;
-    alert(error.response.data.message);
-  }
-
-  if (friends.data) {
-    const data = friends.data.data.data;
-
+  if (!users) {
     return (
-      <aside className="flex-auto">
-        <ul className="max-sm:h-full flex flex-col gap-2 p-2 rounded-2xl border-2 border-(--glass-border-dark) bg-(--glass-fill-dark) backdrop-blur-(--glass-blur)">
-          <h1 className="text-white text-sm text-center font-[Syncopate] font-bold">
-            Friends
-          </h1>
-          {data[0].followers.map((data: FriendList) => (
-            <MessageUser
-              user={data}
-              key={data.id}
-              menu={userMenu}
-              handleUserMenuHighlight={handleUserMenuHighlight}
-            />
-          ))}
-          {data[0].myFriends.map((data: FriendList) => (
-            <MessageUser
-              user={data}
-              key={data.id}
-              menu={userMenu}
-              handleUserMenuHighlight={handleUserMenuHighlight}
-            />
-          ))}
+      <aside className="flex-auto min-h-0 flex flex-col gap-2 p-2 rounded-2xl border-2 border-(--glass-border-dark) bg-(--glass-fill-dark) backdrop-blur-(--glass-blur)">
+        <ul>
+          <ActiveUserLoader />
+          <ActiveUserLoader />
+          <ActiveUserLoader />
         </ul>
+      </aside>
+    );
+  }
+
+  if (users) {
+    return (
+      <aside className="flex-auto min-h-0 flex flex-col gap-2 p-2 rounded-2xl border-2 border-(--glass-border-dark) bg-(--glass-fill-dark) backdrop-blur-(--glass-blur)">
+        <h1 className="text-white text-sm text-center font-[Syncopate] font-bold">
+          Friends
+        </h1>
+        <div className="min-h-0 overflow-auto">
+          {" "}
+          <ul className="flex flex-col gap-2">
+            {users.map((data: FriendList) => (
+              <MessageUser
+                user={data}
+                key={data.id}
+                menu={userMenu}
+                handleUserMenuHighlight={handleUserMenuHighlight}
+              />
+            ))}
+          </ul>
+        </div>
       </aside>
     );
   }
