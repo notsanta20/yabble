171408ff14849227d7a5713c9fcba1190506bf9c
santhosh <santhosh.003z@gmail.com>
2025-05-31 16:47:08 +0530

    fix: fixed overflow issue

--- src/app/routes/app/UserProfile.tsx
+++ src/app/routes/app/UserProfile.tsx
@@ -6,7 +6,7 @@ function UserProfile() {
   return (
     <main className="flex flex-col gap-2 p-2 h-dvh">
       <HeadingSmall />
-      <div className="flex-1 flex flex-col-reverse md:flex-row gap-2">
+      <div className="flex-1 flex flex-col-reverse md:flex-row gap-2 min-h-0">
         <Menu name="null" />
         <UserProfileView />
       </div>
