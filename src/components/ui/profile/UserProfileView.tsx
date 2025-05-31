27ca452bc0bd8856b6284ff6dce623b6c81d6dd3
santhosh <santhosh.003z@gmail.com>
2025-05-31 16:39:57 +0530

    feat: added socket connect on disconnection and removed edit modal

--- src/components/ui/profile/UserProfileView.tsx
+++ src/components/ui/profile/UserProfileView.tsx
@@ -2,18 +2,20 @@ import RequestButton from "../buttons/RequestButton";
 import UserPic from "./UserPic";
 import ButtonFunc from "../buttons/ButtonFunc";
 import UserSection from "./UserSections";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { getHeader, getUserApi } from "../../../utils/apis/getRequests";
 import { useQuery } from "@tanstack/react-query";
-import EditModal from "../modal/EditModal";
 import ProfileLoader from "../loaders/ProfileLoader";
 import { useParams } from "react-router";
+import socket from "../../../app/socket";
 
 function UserProfileView() {
-  const dialogRef = useRef<HTMLDialogElement | null>(null);
   const [page, setPage] = useState("Posts");
   const { userId } = useParams();
   const header = getHeader();
+  const token = header.headers.Authorization;
+  socket.auth = { token };
+  socket.connect();
 
   const user = useQuery({
     queryKey: ["user"],
@@ -36,8 +38,8 @@ function UserProfileView() {
     const userData = user.data.data.data;
 
     return (
-      <section className="flex-auto flex items-center md:justify-center text-white">
-        <div className="min-w-full md:min-w-[50%] h-full flex flex-col gap-5 py-3 px-2 md:px-15 rounded-2xl border-2 border-(--glass-border-light) bg-(--glass-fill-light) backdrop-blur-(--glass-blur)">
+      <section className="flex-auto flex items-center md:justify-center text-white min-h-0">
+        <div className="w-full md:min-w-[400px] md:max-w-[60%] h-full flex flex-col md:items-center gap-5 py-3 px-2 md:px-15 rounded-2xl border-2 border-(--glass-border-light) bg-(--glass-fill-light) backdrop-blur-(--glass-blur)">
           <div className="md:min-w-[400px] flex gap-4 items-center mt-[30px] py-5 px-3 rounded-2xl border-2 border-(--glass-border-dark) bg-(--glass-fill-dark)">
             <UserPic user={userData} />
             <div className="flex-auto flex flex-col justify-around">
@@ -70,11 +72,10 @@ function UserProfileView() {
               isPending={false}
             />
           </div>
-          <div className="md:min-w-[400px] flex-auto">
+          <div className="md:min-w-[400px] flex-auto min-h-0 overflow-auto">
             <UserSection page={page} user={userData} />
           </div>
         </div>
-        <EditModal ref={dialogRef} />
       </section>
     );
   }
