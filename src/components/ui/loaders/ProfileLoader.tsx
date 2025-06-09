e8bc729adb62dac5ab5441bf35f1ea0d32b7a08d
santhosh <santhosh.003z@gmail.com>
2025-06-10 00:05:04 +0530

    fix: fixed styling for mobile view

--- src/components/ui/loaders/ProfileLoader.tsx
+++ src/components/ui/loaders/ProfileLoader.tsx
@@ -4,9 +4,9 @@ import PostCompactLoader from "./PostCompactLoader";
 
 function ProfileLoader() {
   return (
-    <section className="flex-auto flex items-center justify-center text-white">
+    <section className="flex-auto flex items-center md:justify-center text-white min-h-0">
       <SkeletonTheme baseColor="#202020" highlightColor="#444">
-        <div className="w-full md:min-w-[400px] md:max-w-[60%] h-full flex flex-col gap-5 py-3 px-15 rounded-2xl border-2 border-(--glass-border-light) bg-(--glass-fill-light) backdrop-blur-(--glass-blur)">
+        <div className="w-full md:min-w-[400px] md:max-w-[60%] h-full flex flex-col md:items-center gap-5 py-3 px-2 md:px-15 rounded-2xl border-2 border-(--glass-border-light) bg-(--glass-fill-light) backdrop-blur-(--glass-blur)">
           <div className="w-full md:min-w-[400px] flex gap-4 items-center mt-[30px] py-5 px-3 rounded-2xl border-2 border-(--glass-border-dark) bg-(--glass-fill-dark)">
             <Skeleton circle={true} width="100px" height="100px" />
             <div className="flex-auto flex flex-col justify-around">
